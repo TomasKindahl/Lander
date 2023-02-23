e81b90db813c5c8bd211af9c5923c96a60ad9769
Tomas Kindahl <tomas.kindahl@bahnhof.se>
2023-02-23 14:54:05 +0100

    Lade till variabler sverige, tyskland, sanMarino

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace D14_ovn_1_1_uppg_8t15
+﻿using System.Xml;
+
+namespace D14_ovn_1_1_uppg_8t15
 {
     internal class Program
     {
@@ -10,7 +12,28 @@
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Uppg 9:
+            Land sverige = new Land()
+            {
+                namn = "Sverige",
+                styrestyp = "monarki",
+                huvudstad = "Stockholm",
+                invånarantal = 10512820
+            };
+            Land tyskland = new Land()
+            {
+                namn = "Tyskland",
+                styrestyp = "republik",
+                huvudstad = "Berlin",
+                invånarantal = 83783902
+            };
+            Land sanMarino = new Land()
+            {
+                namn = "San Marino",
+                styrestyp = "republik",
+                huvudstad = "San Marino",
+                invånarantal = 33600
+            };
         }
     }
 }
