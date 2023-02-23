200b21a69985e0c393a4196f5eb550797d3fc553
Tomas Kindahl <tomas.kindahl@bahnhof.se>
2023-02-23 14:45:44 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace D14_ovn_1_1_uppg_8t15
+{
+    internal class Program
+    {
+        // Uppg 8:
+        class Land
+        {
+            public string namn, styrestyp, huvudstad;
+            public int invånarantal;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
